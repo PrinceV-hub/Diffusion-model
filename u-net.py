5c9a99bb57bc90049b91d2549f3769a3e52af764
Prince Verma <sm24mtech11003@iith.ac.in>
2025-04-21 01:27:43 +0530

    Create u-net.py

---
+++ u-net.py
@@ -0,0 +1,13 @@
+class SimpleUNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 3, 3, padding=1),
+        )
+
+    def forward(self, x, t):
+        return self.model(x)
